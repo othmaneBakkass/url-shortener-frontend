f80ee36254b966e01c04e378bf0d391d45626a34
othmane bakkass <othmanebakkass@gmail.com>
2024-09-17 18:09:52 +0100

    feat: add create short link form

---
+++ src/components/blocks/home/form/index.tsx
@@ -0,0 +1,82 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+	name: z.string().min(2, {
+		message: "name must be at least 2 characters.",
+	}),
+	url: z.string().min(2).url(),
+});
+
+export function CreateShortLinkForm() {
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: "",
+			url: "",
+		},
+	});
+
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	}
+
+	return (
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="flex gap-y-4 flex-col max-w-96 p-4 rounded-2xl border border-neutral-800">
+				<FormField
+					control={form.control}
+					name="name"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>name</FormLabel>
+							<FormControl>
+								<Input placeholder="short link name" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="url"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>url</FormLabel>
+							<FormControl>
+								<Input
+									type="url"
+									placeholder="https://example.com"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button className="self-end" type="submit">
+					create short link
+				</Button>
+			</form>
+		</Form>
+	);
+}
---
+++ src/routes/home.tsx
@@ -0,0 +1,9 @@
+import { CreateShortLinkForm } from "@/components/blocks/home/form";
+
+export function Home() {
+	return (
+		<>
+			<CreateShortLinkForm />
+		</>
+	);
+}
