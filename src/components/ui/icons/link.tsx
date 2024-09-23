d562b10cac53f4ae9361071cc282826a7bde9c53
othmane bakkass <othmanebakkass@gmail.com>
2024-09-23 20:17:46 +0100

    bug: correct some props naming

--- src/components/ui/icons/link.tsx
+++ src/components/ui/icons/link.tsx
@@ -10,9 +10,9 @@ export function LinkIcon({ className }: { className?: string }) {
 				viewBox="0 0 24 24"
 				fill="none"
 				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 				className={cn(
 					"text-foreground size-4 hover:text-card-foreground",
 					className
