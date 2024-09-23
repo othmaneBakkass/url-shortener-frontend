5e15596dbb6198dfbfdb124aaa26cdf0ab7520be
othmane bakkass <othmanebakkass@gmail.com>
2024-09-23 20:15:21 +0100

    feat: add sleep function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,36 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+/**
+ * The `sleep` function is a function that returns a promise that resolves or reject after a specified
+ * amount of time.
+ *
+ * @param {sleepProps} [options]
+ * @default {time:10000ms,resolve:true,response:{ok:true}
+ *
+ */
+export function sleep<Res extends sleepResponse = { ok: boolean }>(
+	options: sleepProps<Res>
+): sleepRT<Res> {
+	const { time = 10_000, resolve = true, response } = options;
+
+	return new Promise((res, rej) => {
+		setTimeout(() => {
+			resolve
+				? res(response ?? ({ ok: true } as Res))
+				: rej(response ?? { ok: false });
+		}, time);
+	});
+}
+
+type sleepProps<T> = {
+	time?: number;
+	resolve?: boolean;
+	response?: T;
+};
+type sleepResponse = { ok: boolean; [key: string]: any };
+type sleepRT<T> = Promise<T>;
